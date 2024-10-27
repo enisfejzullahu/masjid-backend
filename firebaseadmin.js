cdcbbea2065f4b234869a94324c256a3127e387f
efejzullahu <enisfejzullahu04@gmail.com>
2024-10-27 15:44:07 +0100

    Fixed FirebaseAdmin

--- firebaseadmin.js
+++ firebaseadmin.js
@@ -1,20 +1,7 @@
-// const admin = require("firebase-admin");
-
-// // Parse the service account key from the environment variable
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL:
-//     "https://xhamia-ime-8e033-default-rtdb.europe-west1.firebasedatabase.app",
-// });
-
-// const db = admin.firestore();
-
-// module.exports = { db }; // Only export the database
-
 const admin = require("firebase-admin");
-const serviceAccount = require("./xhamia-ime-8e033-firebase-adminsdk-joivd-1e730a6474.json");
+
+// Parse the service account key from the environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -23,17 +10,30 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
+
+// module.exports = { db }; // Only export the database
+
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./xhamia-ime-8e033-firebase-adminsdk-joivd-1e730a6474.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL:
+//     "https://xhamia-ime-8e033-default-rtdb.europe-west1.firebasedatabase.app",
+// });
+
+// const db = admin.firestore();
 const realtimeDatabase = admin.database(); // This initializes the Realtime Database
 
 // Example: Fetch prayer times from Realtime Database
 const getTodayPrayerTimes = async () => {
   try {
     // Fetch all prayer times from the root of the Realtime Database
-    const snapshot = await realtimeDatabase.ref('/').once('value');
-    
+    const snapshot = await realtimeDatabase.ref("/").once("value");
+
     // Log the snapshot value to inspect the structure
     const prayerTimesArray = snapshot.val();
-    
+
     // Get today's date
     const today = new Date();
     const muaji = today.getMonth() + 1; // Months are 0-based in JS
@@ -47,21 +47,18 @@ const getTodayPrayerTimes = async () => {
     if (todayPrayerTimes) {
       return todayPrayerTimes;
     } else {
-      console.log('No prayer times found for today.');
+      console.log("No prayer times found for today.");
       return null;
     }
   } catch (error) {
-    console.error('Error fetching prayer times:', error);
+    console.error("Error fetching prayer times:", error);
     return null;
   }
 };
-
-
-
 
 // Run the function to test it
 // getTodayPrayerTimes();
 
 // const firebaseAdmin = db;
 
-module.exports = {db, getTodayPrayerTimes};
+module.exports = { db, getTodayPrayerTimes };
